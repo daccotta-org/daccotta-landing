0293d3d68923f087297982a14f7ea25bfb605c3f
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-09-23 22:42:36 +0530

    fixed responsiveness

--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-[60vh] md:h-[40rem]  bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800  flex items-center w-full justify-center overflow-hidden",
+        "h-[60vh] md:h-[40rem]  bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800  flex items-center w-screen justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}
