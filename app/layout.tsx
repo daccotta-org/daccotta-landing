b1a81c80f56b1097f34d6aa3a2d8e8f87af3ffa8
Ashu Kumar <110407868+AshuKr22@users.noreply.github.com>
2024-09-28 21:23:04 +0530

    seo updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,51 @@
 import "./globals.css";
 import favicon from "../assets/favicon.ico";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Daccotta | The Social Network For Movie Lovers",
+  description:
+    "Daccotta is a social network for movie lovers. Create lists, journal your movie experiences, get personalized recommendations, and connect with fellow film enthusiasts.",
+  keywords:
+    "movies, social network, film, cinema, movie lists, movie journal, recommendations",
+  openGraph: {
+    title: "Daccotta | The Social Network For Movies",
+    description:
+      "Connect with movie lovers, create lists, and journal your film experiences.",
+    url: "https://www.daccotta.com", // Replace with your actual URL
+    siteName: "Daccotta",
+    images: [
+      {
+        url: "/dacotta-social.png", // Path relative to the 'public' folder
+        width: 1200,
+        height: 400,
+        alt: "Daccotta - The Social Network For Movie Lovers",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@daccotta", // Replace with your Twitter handle
+    creator: "@daccotta",
+    title: "Daccotta | The Social Network For Movies",
+    description:
+      "Discover, share, and connect through your love of cinema. Create lists, journal entries, and get personalized movie recommendations.",
+    images: ["/dacotta-social.png"], // Path relative to the 'public' folder
+  },
+  other: {
+    "linkedin:card": "summary_large_image",
+    "linkedin:title": "Daccotta | Connect Through Cinema",
+    "linkedin:description":
+      "Join Daccotta to create movie lists, journal your film experiences, and get personalized recommendations. The ultimate social network for movie enthusiasts.",
+    "linkedin:image": "/dacotta-social.png",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <title>Daccotta | The Social Network For Movies</title>
+        <title>Daccotta | The Social Network For Movie Lovers</title>
         <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
       </head>
       <body>{children}</body>
